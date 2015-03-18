9df7f5861b04fe0d935488aeba42b651e28aa868
Greg DeAngelis <gdeangel@gmail.com>
2015-03-18 14:58:56 -0400

    Properly handle transitions to airplane mode

--- src/android/ConnectionValidityChecker.java
+++ src/android/ConnectionValidityChecker.java
@@ -1,9 +1,12 @@
 package org.aquto.cordova.vpn;
 
+import android.annotation.TargetApi;
 import android.content.*;
 import android.content.pm.*;
 import android.net.*;
 import android.os.Bundle;
+import android.os.Build;
+import android.provider.Settings;
 import android.util.Log;
 import java.util.*;
 import org.strongswan.android.logic.CharonVpnService;
@@ -38,22 +41,31 @@ public class ConnectionValidityChecker extends BroadcastReceiver {
     }
 
     public void register() {
-        if(!allowWiFi)
-            mContext.registerReceiver(this, new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(ConnectivityManager.CONNECTIVITY_ACTION);
+        filter.addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED);
+        mContext.registerReceiver(this, filter);
     }
 
     public void unregister() {
-        if(!allowWiFi)
-            mContext.unregisterReceiver(this);
+        mContext.unregisterReceiver(this);
+    }
+
+    @SuppressWarnings("deprecation")
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
+    private boolean isAirplaneModeOn() {
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR1)
+            return Settings.System.getInt(mContext.getContentResolver(), Settings.System.AIRPLANE_MODE_ON, 0) != 0;
+        else
+            return Settings.Global.getInt(mContext.getContentResolver(), Settings.Global.AIRPLANE_MODE_ON, 0) != 0;
     }
 
     public boolean connectionValid() {
-        if(allowWiFi)
-            return true;
         ConnectivityManager cm = (ConnectivityManager)mContext.getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo info = cm.getActiveNetworkInfo();
-        //assume null connection info to be valid
-        return info == null || !disallowedNetworkTypes.contains(info.getType());
+        if(info == null)
+            return false;
+        return (allowWiFi || !disallowedNetworkTypes.contains(info.getType())) && !isAirplaneModeOn();
     }
 
     private void stopVpn() {
